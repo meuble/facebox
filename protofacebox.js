0231e331fd8859408265cfddb61419273eb1444c
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2009-05-07 01:12:15 +0200

    Improuvments on the ProtoFacebook class. Now display correctly any type of popups (text, div, pictures). Ajax is still missing....

--- protofacebox.js
+++ protofacebox.js
@@ -1,13 +1,13 @@
 var ProtoFacebox = Class.create({
-	initialize: function(expression) {
+	initialize: function(data, klass, options) {
 		this.settings = {
 			opacity      : 0,
-			overlay      : true,
-			loadingImage : '/facebox/loading.gif',
-			closeImage   : '/facebox/closelabel.gif',
+			overlay      : false,
+			loadingImage : 'file:///Users/sakkaoui/Documents/code/facebox/loading.gif',
+			closeImage   : 'file:///Users/sakkaoui/Documents/code/facebox/closelabel.gif',
 			imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
 			faceboxHtml  : '\
-    <div id="facebox" style="display:none;"> \
+    <div id="protofacebox" style="display:none;"> \
       <div class="popup"> \
         <table id="protofacebox_table"> \
           <tbody> \
@@ -20,8 +20,8 @@ var ProtoFacebox = Class.create({
                 <div id="protofacebox_content"> \
                 </div> \
                 <div class="footer"> \
-                  <a href="#" class="close"> \
-                    <img src="/facebox/closelabel.gif" title="close" class="close_image" /> \
+                  <a href="#" id="protofacebox_close"> \
+                    <img src="/facebox/closelabel.gif" title="close" id="protofacebox_close_image" /> \
                   </a> \
                 </div> \
               </td> \
@@ -35,23 +35,32 @@ var ProtoFacebox = Class.create({
       </div> \
     </div>'
 		};
-		// this.init();
-		this.loading();
+		this.init();
+		
+		if (data) {
+			this.loading();
+			if (data.ajax) this.fillFaceboxFromAjax(data.ajax, klass);
+		    else if (data.image) this.fillFaceboxFromImage(data.image, klass);
+		    else if (Object.isFunction(data)) data.call(this);
+		    else this.reveal(data, klass);
+		}
 	},
 
 	loading: function() {
-		this.init();
+		// this.init();
 		if ($('protofacebox_loading') != null) return true;
 		this.showOverlay();
+		
+		$('protofacebox_content').update();
+		$('protofacebox_body').insert('<div id="protofacebox_loading"><img src="' + this.settings.loadingImage + '" /></div>');
 
-		$('protofacebox_content').empty();
-		$('protofacebox_body').hide().insert('<div id="protofacebox_loading"><img src="' + this.settings.loadingImage + '"/></div>');
-
-		$('facebox').setStyle({
-			'top':	getPageScroll()[1] + (getPageHeight() / 10),
-			'left':	document.width / 2 - 205
+		var top = getPageScroll()[1] + (getPageHeight() / 10);
+		var left = document.width / 2 - 205;
+		$('protofacebox').setStyle({
+			'top':	top + 'px',
+			'left':	left + 'px'
 		}).show();
-
+		
 		// $(document).bind('keydown.facebox', function(e) {
 		// 	if (e.keyCode == 27) $.facebox.close()
 		// 	return true
@@ -62,17 +71,23 @@ var ProtoFacebox = Class.create({
 	reveal: function(data, klass) {
 		$(document).fire('ProtoFacebox:beforeReveal');
 		if (klass) $('protofacebox_content').addClassName(klass);
-		console.log(data);
-		console.log($('protofacebox_content'));
-		$('protofacebox_content').appendChild(data);
-		$('protofacebox_loading').remove();
+		$('protofacebox_content').insert(data);
+		if ($('protofacebox_loading')) $('protofacebox_loading').remove();
 		$('protofacebox_body').appear();
-		$('facebox').setStyle({left: document.width / 2 - ($('protofacebox_table').getDimensions().width / 2)});
+		$('protofacebox').setStyle({left: document.width / 2 - ($('protofacebox_table').getDimensions().width / 2)});
 		$(document).fire('ProtoFacebox:reveal')
 		$(document).fire('ProtoFacebox:afterReveal');
 	},
 	
-	close: function() {},
+	close: function() {
+		// $(document).unbind('keydown.facebox')
+	    $('protofacebox').fade();
+     	this.hideOverlay();
+		if ($('protofacebox_loading') != null) $('protofacebox_loading').remove();
+		
+		$(document).fire('ProtoFacebox:close');
+      	return false;
+	},
 	
 	init: function(settings) {
 		if (this.settings.inited) return true;
@@ -91,13 +106,13 @@ var ProtoFacebox = Class.create({
     	preload[0].src = this.settings.closeImage;
     	preload[1].src = this.settings.loadingImage;
 
-    	$$('#facebox .b:first', '#facebox .bl', '#facebox .br', '#facebox .tl', '#facebox .tr').each(function(e) {
+    	$$('#protofacebox .b:first', '#protofacebox .bl', '#protofacebox .br', '#protofacebox .tl', '#protofacebox .tr').each(function(e) {
       		preload.push(new Image());
       		preload.slice(-1).src = e.getStyle('background-image').replace(/url\((.+)\)/, '$1');
     	})
 
-    	$$('#facebox .close')[0].observe('click', this.close.bind(this));
-    	$$('#facebox .close_image')[0].setAttribute('src', this.settings.closeImage);
+    	$('protofacebox_close').observe('click', this.close.bind(this));
+    	$('protofacebox_close_image').setAttribute('src', this.settings.closeImage);
   	},
 
   	skipOverlay: function() {
@@ -107,45 +122,79 @@ var ProtoFacebox = Class.create({
   	showOverlay: function() {
     	if (this.skipOverlay()) return;
 
-    	if ($('facebox_overlay') == null)
-      		$$("body")[0].insert('<div id="facebox_overlay" class="facebox_hide"></div>');
+    	if ($('protofacebox_overlay') == null)
+      		$$("body")[0].insert('<div id="protofacebox_overlay" class="protofacebox_hide"></div>');
 
-    	$('facebox_overlay').hide().addClassName("facebox_overlayBG")
-      		.setOpacity(this.settings.opacity)
-      		.observe('click', function() { $(document).fire('ProtoFacebox:close') })
-      		.appear();
+		$('protofacebox_overlay').hide();
+    	$('protofacebox_overlay').addClassName("protofacebox_overlayBG")
+			.setOpacity(0.5)
+			.observe('click', function() { $(document).fire('ProtoFacebox:close') })
+			.appear({to: this.settings.opacity, duration: 5});
     	return false;
-  }
+	},
+	
+	hideOverlay: function() {
+		if (this.skipOverlay()) return;
 
-  
+    	$('protofacebox_overlay').fade({duration: 0.2});
+   		$("protofacebox_overlay").removeClassName("protofacebox_overlayBG");
+   		$("protofacebox_overlay").addClassName("protofacebox_hide");
+   		$("protofacebox_overlay").remove();
+
+    	return false;
+  	},
+
+  	fillFaceboxFromAjax: function(url, klass) {
+		new Ajax.Request(url, {
+		  method: 'get',
+		  onSuccess: function(transport) {
+			this.reveal(transport.responseText, klass);
+		  }
+		});
+  	},
+
+  	fillFaceboxFromImage: function(href, klass) {
+    	var image = new Image();
+		
+		var f = function() {
+      		this.reveal('<div class="image"><img src="' + image.src + '" /></div>', klass);
+    	}
+
+    	image.onload = f.bind(this)
+    	image.src = href;
+  	}
 });
 
 
 // getPageScroll() by quirksmode.com
 function getPageScroll() {
-  var xScroll, yScroll;
-  if (self.pageYOffset) {
-    yScroll = self.pageYOffset;
-    xScroll = self.pageXOffset;
-  } else if (document.documentElement && document.documentElement.scrollTop) {	 // Explorer 6 Strict
-    yScroll = document.documentElement.scrollTop;
-    xScroll = document.documentElement.scrollLeft;
-  } else if (document.body) {// all other Explorers
-    yScroll = document.body.scrollTop;
-    xScroll = document.body.scrollLeft;
-  }
-  return new Array(xScroll,yScroll)
+  	var xScroll, yScroll;
+
+  	if (self.pageYOffset) {
+    	yScroll = self.pageYOffset;
+    	xScroll = self.pageXOffset;
+	} else if (document.documentElement && document.documentElement.scrollTop) {	 // Explorer 6 Strict
+    	yScroll = document.documentElement.scrollTop;
+    	xScroll = document.documentElement.scrollLeft;
+  	} else if (document.body) {// all other Explorers
+		yScroll = document.body.scrollTop;
+    	xScroll = document.body.scrollLeft;
+  	}
+
+  	return new Array(xScroll,yScroll);
 }
 
 // Adapted from getPageSize() by quirksmode.com
 function getPageHeight() {
-  var windowHeight
-  if (self.innerHeight) {	// all except Explorer
-    windowHeight = self.innerHeight;
-  } else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
-    windowHeight = document.documentElement.clientHeight;
-  } else if (document.body) { // other Explorers
-    windowHeight = document.body.clientHeight;
-  }
-  return windowHeight
+  	var windowHeight;
+
+  	if (self.innerHeight) {	// all except Explorer
+    	windowHeight = self.innerHeight;
+  	} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+    	windowHeight = document.documentElement.clientHeight;
+  	} else if (document.body) { // other Explorers
+    	windowHeight = document.body.clientHeight;
+  	}
+
+  	return windowHeight;
 }
